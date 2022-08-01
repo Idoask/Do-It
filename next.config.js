4fd97abf0f98a5da83d4b53520a655ddd160b368
dorz <dorz@sling.insure>
2022-08-01 15:12:21 +0300

    fix client id

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   // pageExtensions:['Tasks.tsx','TasksPage.tsx','CheckList.tsx','LinearProgressWithLabel.tsx','AppBar.tsx','TaskCard.tsx'],
   env:{
     siteUrl:'https://do-it-project-six.vercel.app',
-    NEXT_PUBLIC_CLIENT_ID:'719797199893-br8esks29ts1ih0t3u5fd9ep22ln5rfc.apps.googleusercontent.com'
+    NEXT_PUBLIC_CLIENT_ID:'535846742868-guptiq92rmta181m6o53rtdic29km3v1.apps.googleusercontent.com'
 
   }
 }
