9b60a5e5796e7d7557e9d53cf78847a99afd4310
dorz <dorz@sling.insure>
2022-08-01 15:36:45 +0300

    fix site

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   // pageExtensions:['Tasks.tsx','TasksPage.tsx','CheckList.tsx','LinearProgressWithLabel.tsx','AppBar.tsx','TaskCard.tsx'],
   env:{
-    siteUrl:'https://do-it-project-six.vercel.app',
+    siteUrl:'do-it-azure.vercel.app',
     NEXT_PUBLIC_CLIENT_ID:'535846742868-guptiq92rmta181m6o53rtdic29km3v1.apps.googleusercontent.com'
 
   }
